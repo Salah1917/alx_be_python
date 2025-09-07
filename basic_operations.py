5d17b493e48793082dd776ce274c6903a32a6538
Salah1917 <salahmagdy2004@gmail.com>
2025-09-08 02:31:49 +0300

    Add basic operartions task

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+# Addition of 10 and 5 is 15
+# Subtraction of 10 and 5 is 5
+# Multiplication of 10 and 5 is 50
+
+print("Addition of", 10 , "and", 15 ,"is", 10+5)
+
+print("Subtraction of", 10 , "and", 15 ,"is", 10-5)
+
+print("Multiplication of", 10 , "and", 15 ,"is", 10*5)
