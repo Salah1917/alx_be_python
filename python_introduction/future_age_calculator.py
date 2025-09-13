a026a8d2e42d4c467839d05ef01ea06ed67fa070
Salah1917 <salahmagdy2004@gmail.com>
2025-09-13 22:16:13 +0300

    Add files

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,17 @@
+# How old are you? ”.
+# Assume the user will input a valid integer value.
+# Calculate how old the user will be in the year 2050. To keep calculations simple, assume the current year is 2023. Therefore, you need to add 27 years to the user’s current age.
+# Print the user’s age in 2050 in the format: In 2050, you will be [age] years old.
+# Expected Script Flow:
+
+# The script prompts the user for their current age.
+# The user enters their age.
+# The script calculates and prints how old the user will be in 2050.
+# Example Interaction:
+
+# If the user inputs 30 when prompted for their current age, the output should be:
+
+# In 2050, you will be 57 years old.
+
+age = input("How old are you?")
+print(f"in 2050 you will be {age + 27} years old." )
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,17 @@
+# principal with a value of 1000 (representing $1000),
+# rate with a value of 0.05 (representing 5% annual interest rate),
+# time with a value of 3 (representing 3 years).
+# Calculate the simple interest using the formula provided and store the result in a variable named interest.
+# Print the calculated interest in a format: The simple interest is: [interest].
+# Expected Output:
+
+# When executed, your script should print the simple interest calculated with the provided values. For principal = 1000, rate = 0.05, and time = 3, the output should be:
+
+# The simple interest is: 150.0
+
+
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time 
+print(f"The simple interest is: {interest}")
