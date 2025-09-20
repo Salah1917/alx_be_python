27382c160b6f5c31af507368ea2ea0f7483ffc3e
Salah1917 <salahmagdy2004@gmail.com>
2025-09-21 01:58:39 +0300

    Fix bugs

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,7 @@
 number = int(input("Enter the size of the pattern:"))
 x = 0
-while(x < number):
-    for i in range (1, number+1, 1):
+while x < number:
+    for i in range (1, number + 1):
         print("*", end="")
     print()
     x = x + 1
