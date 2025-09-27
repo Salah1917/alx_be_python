37c74dc18d016ddc15c8b75931c8109a9d1ff427
Salah1917 <salahmagdy2004@gmail.com>
2025-09-27 20:05:53 +0300

    Fix bug

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,11 +12,11 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter the name of the item you want add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             pass
         elif choice == '2':
-            item = input("Enter the name of the item you want to remove: ")
+            item = input("Enter the item to remove: ")
             shopping_list.remove(item)
             pass
         elif choice == '3':
